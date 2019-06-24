67c15993971370e43e582ed486e51c6c7257793a
Andrei Badea <zbiller@gmail.com>
2019-06-24 22:16:01 +0300

    added admin reset password notification

---
+++ src/Notifications/ResetPassword.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Varbox\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPassword extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * The user's reset password token.
+     *
+     * @var string
+     */
+    public $token;
+
+    /**
+     * Set the token and the route.
+     *
+     * @param string $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Establish the notification sending protocol.
+     *
+     * @param mixed $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return [
+            'mail',
+        ];
+    }
+
+    /**
+     * @param mixed $notifiable
+     * @return mixed
+     */
+    public function toMail($notifiable)
+    {
+        if (\Varbox::moduleEnabled('cms')) {
+            $types = app('email.model')->getTypes();
+
+            if (array_key_exists('password-recovery', $types)) {
+                $class = $types['password-recovery']['class'] ?? 'Varbox\Cms\Mail\PasswordRecovery';
+
+                return (new $class($notifiable, $this->token))->to($notifiable->email);
+            }
+        }
+
+        return (new MailMessage)
+            ->subject(__('Reset Password Notification'))
+            ->line(__('You are receiving this email because we received a password reset request for your account.'))
+            ->action(__('Reset Password'), url(config('app.url') . route($notifiable->isAdmin() ? 'admin.password.reset' : 'password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
+            ->line(__('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.users.expire')]))
+            ->line(__('If you did not request a password reset, no further action is required.'));
+    }
+}
