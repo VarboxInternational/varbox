78d77f052a908b271db1db1d0c4b2366211fbfd1
Andrei Badea <zbiller@gmail.com>
2019-06-24 21:43:12 +0300

    created roles seeder

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Varbox\Seed;
+
+use Illuminate\Database\Seeder;
+use Varbox\Contracts\PermissionModelContract;
+use Varbox\Contracts\RoleModelContract;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Mapping structure of admin roles.
+     *
+     * @var array
+     */
+    private $roles = [
+        'Admin' => [
+            'name' => 'Admin',
+            'guard' => 'admin',
+        ],
+        'Super' => [
+            'name' => 'Super',
+            'guard' => 'admin',
+        ],
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @param RoleModelContract $role
+     * @return void
+     */
+    public function run(RoleModelContract $role)
+    {
+        foreach ($this->roles as $label => $data) {
+            if ($role->where('name', $data['name'])->count() == 0) {
+                $role->create($data);
+            }
+        }
+    }
+}
