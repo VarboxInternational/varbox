45ad147902db882caa786aaa3c2f356e43a4b406
Andrei Badea <zbiller@gmail.com>
2019-06-24 22:11:27 +0300

    added role filter

---
+++ src/Filters/RoleFilter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Varbox\Filters;
+
+class RoleFilter extends Filter
+{
+    /**
+     * Get the main where condition between entire request fields.
+     *
+     * @return string
+     */
+    public function morph()
+    {
+        return 'and';
+    }
+
+    /**
+     * Get the filters that apply to the request.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            'search' => 'operator:like|condition:or|columns:name',
+            'guard' => 'operator:=|condition:or|columns:guard',
+            'start_date' => 'operator:date >=|condition:or|columns:created_at',
+            'end_date' => 'operator:date <=|condition:or|columns:created_at',
+        ];
+    }
+
+    /**
+     * Get the modified value of a request filter field.
+     *
+     * @return array
+     */
+    public function modifiers()
+    {
+        return [];
+    }
+}
