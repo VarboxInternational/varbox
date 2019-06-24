110bbe670f43626504a38ae657deb3a3471a3d2d
Andrei Badea <zbiller@gmail.com>
2019-06-24 22:15:43 +0300

    added auth request classes

---
+++ src/Requests/PasswordResetRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Varbox\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PasswordResetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'token' => [
+                'required',
+            ],
+            'email' => [
+                'required',
+                'email',
+            ],
+            'password' => [
+                'required',
+                'confirmed',
+            ]
+        ];
+    }
+}
