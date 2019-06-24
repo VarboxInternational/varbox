ba3c201d06fdc80dfe6c7ae280721d4fda469242
Andrei Badea <zbiller@gmail.com>
2019-06-24 21:39:49 +0300

    added varbox breadcrumb config file

---
+++ config/varbox-breadcrumb.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|
+| This is a wrapper around the "davejamesmiller/laravel-breadcrumbs" package.
+| For full documentation please refer to: https://github.com/davejamesmiller/laravel-breadcrumbs
+|
+*/
+
+return [
+
+    /*
+    |
+    | Flag indicating whether or not breadcrumbs should be enabled in the entire admin.
+    |
+    */
+    'enable' => true,
+
+    /*
+    |
+    | Determine whether or not to throw exceptions when some breadcrumb setup is wrong.
+    |
+    | An exception will be thrown in one of the following scenarios:
+    | - when route-bound breadcrumbs are used but the current route doesn't have a name (UnnamedRouteException)
+    | - when route-bound breadcrumbs are used and the matching breadcrumb doesn't exist (InvalidBreadcrumbException)
+    | - when a named breadcrumb is used but doesn't exist (InvalidBreadcrumbException)
+    |
+    */
+    'throw_exceptions' => true,
+
+];
