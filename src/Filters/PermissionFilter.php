db57297b8d729fc24838e449a15c56310e2e8c1a
Andrei Badea <zbiller@gmail.com>
2019-06-24 22:10:24 +0300

    added permission filter

---
+++ src/Filters/PermissionFilter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Varbox\Filters;
+
+class PermissionFilter extends Filter
+{
+    /**
+     * Get the main where condition between entire request fields.
+     *
+     * @return string
+     */
+    public function morph()
+    {
+        return 'and';
+    }
+
+    /**
+     * Get the filters that apply to the request.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            'start_date' => 'operator:date >=|condition:or|columns:created_at',
+            'end_date' => 'operator:date <=|condition:or|columns:created_at',
+            'search' => 'operator:like|condition:or|columns:name,group,label',
+            'guard' => 'operator:=|condition:or|columns:guard',
+        ];
+    }
+
+    /**
+     * Get the modified value of a request filter field.
+     *
+     * @return array
+     */
+    public function modifiers()
+    {
+        return [];
+    }
+}
