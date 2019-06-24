cb984c79540568a4398186935b594154953d82e9
Andrei Badea <zbiller@gmail.com>
2019-06-24 22:12:03 +0300

    added admin filter

---
+++ src/Filters/AdminFilter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Varbox\Filters;
+
+class AdminFilter extends Filter
+{
+    /**
+     * Get the main where condition between entire request fields.
+     *
+     * @return string
+     */
+    public function morph()
+    {
+        return 'and';
+    }
+
+    /**
+     * Get the filters that apply to the request.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            'search' => 'operator:like|condition:or|columns:email,first_name,last_name',
+            'active' => 'operator:=|condition:or|columns:active',
+            'role' => 'operator:=|condition:or|columns:roles.role_id',
+            'start_date' => 'operator:date >=|condition:or|columns:users.created_at',
+            'end_date' => 'operator:date <=|condition:or|columns:users.created_at',
+        ];
+    }
+
+    /**
+     * Get the modified value of a request filter field.
+     *
+     * @return array
+     */
+    public function modifiers()
+    {
+        return [];
+    }
+}
