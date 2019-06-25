c7b05294a0e81f4e2a24d75c8fb0da3b5449d896
Andrei Badea <zbiller@gmail.com>
2019-06-25 19:16:50 +0300

    added activity filter

---
+++ src/Filters/ActivityFilter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Varbox\Filters;
+
+class ActivityFilter extends Filter
+{
+    /**
+     * Get the main where condition between entire request fields.
+     *
+     * @return string
+     */
+    public function morph()
+    {
+        return 'and';
+    }
+
+    /**
+     * Get the filters that apply to the request.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            'user' => 'operator:=|condition:or|columns:user_id',
+            'entity' => 'operator:=|condition:or|columns:entity_type',
+            'event' => 'operator:=|condition:or|columns:event',
+            'start_date' => 'operator:date >=|condition:or|columns:created_at',
+            'end_date' => 'operator:date <=|condition:or|columns:created_at',
+        ];
+    }
+
+    /**
+     * Get the modified value of a request filter field.
+     *
+     * @return array
+     */
+    public function modifiers()
+    {
+        return [];
+    }
+}
