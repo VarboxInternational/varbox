8a1e6542d9ba484a2f6bed3270b2788c943c971a
Andrei Badea <zbiller@gmail.com>
2019-06-27 00:10:52 +0300

    fixed menu display on mobile

--- resources/views/layouts/admin/partials/_menu.blade.php
+++ resources/views/layouts/admin/partials/_menu.blade.php
@@ -1,8 +1,8 @@
-<ul class="nav nav-tabs border-0 flex-column flex-lg-row">
+<ul class="nav nav-tabs border-0 flex-column flex-lg-row px-3">
     @foreach($menu->roots() as $item)
         @php($children = $menu->children($item))
-        <li class="nav-item @if($loop->first) pr-0 @else px-0 @endif">
-            <a href="{!! $item->url() ?: 'javascript:void(0)' !!}" class="nav-link px-3 {!! $item->active() ? 'active' : '' !!}" {!! $children->count() ? 'data-toggle="dropdown"' : '' !!}>
+        <li class="nav-item px-0 d-sm-flex">
+            <a href="{!! $item->url() ?: 'javascript:void(0)' !!}" class="nav-link px-3 w-100 {!! $item->active() ? 'active' : '' !!}" {!! $children->count() ? 'data-toggle="dropdown"' : '' !!}>
                 <i class="fa {!! $item->data('icon') !!}"></i>&nbsp; {{ $item->name() }}
             </a>
             @if($children->count())
