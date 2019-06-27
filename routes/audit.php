31aa932f50f4fbe332c94c6c8f549ba3d0b16747
Andrei Badea <zbiller@gmail.com>
2019-06-28 01:28:57 +0300

    attached notifications-read permissions to matching routes

--- routes/audit.php
+++ routes/audit.php
@@ -24,9 +24,9 @@ Route::group([
         'prefix' => 'notifications',
     ], function () use ($controllers) {
         Route::get('/', ['as' => 'admin.notifications.index', 'uses' => $controllers['notifications'] . '@index', 'permissions' => 'notifications-list']);
-        Route::get('action/{notification?}', ['as' => 'admin.notifications.action', 'uses' => $controllers['notifications'] . '@actionNotification']);
-        Route::put('read/{notification}', ['as' => 'admin.notifications.mark_as_read', 'uses' => $controllers['notifications'] . '@markAsRead']);
-        Route::post('read-all', ['as' => 'admin.notifications.mark_all_as_read', 'uses' => $controllers['notifications'] . '@markAllAsRead']);
+        Route::get('action/{notification?}', ['as' => 'admin.notifications.action', 'uses' => $controllers['notifications'] . '@actionNotification', 'permissions' => 'notifications-read']);
+        Route::put('read/{notification}', ['as' => 'admin.notifications.mark_as_read', 'uses' => $controllers['notifications'] . '@markAsRead', 'permissions' => 'notifications-read']);
+        Route::post('read-all', ['as' => 'admin.notifications.mark_all_as_read', 'uses' => $controllers['notifications'] . '@markAllAsRead', 'permissions' => 'notifications-read']);
         Route::delete('destroy/{notification}', ['as' => 'admin.notifications.destroy', 'uses' => $controllers['notifications'] . '@destroy', 'permissions' => 'notifications-delete']);
         Route::delete('delete-read', ['as' => 'admin.notifications.delete_read', 'uses' => $controllers['notifications'] . '@deleteOnlyRead', 'permissions' => 'notifications-delete']);
         Route::delete('delete-old', ['as' => 'admin.notifications.delete_old', 'uses' => $controllers['notifications'] . '@deleteOnlyOld', 'permissions' => 'notifications-delete']);
