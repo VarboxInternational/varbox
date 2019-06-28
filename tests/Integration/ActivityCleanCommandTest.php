548328f383b41d9e9a2c51fcd552f31e2b54e261
Andrei Badea <zbiller@gmail.com>
2019-06-28 23:53:42 +0300

    created tests for the command to delete old activity logs

---
+++ tests/Integration/ActivityCleanCommandTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Varbox\Tests\Integration;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Varbox\Models\Activity;
+
+class ActivityCleanCommandTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpTestingConditions();
+    }
+
+    /** @test */
+    public function it_can_delete_old_activity_logs()
+    {
+        $this->app['config']->set('varbox.varbox-activity.old_threshold', 30);
+
+        $this->assertEquals(4, Activity::count());
+
+        $this->artisan('varbox:clean-activity');
+
+        $this->assertEquals(1, Activity::count());
+    }
+
+    /** @test */
+    public function it_doesnt_delete_any_activity_logs_if_the_days_threshold_is_null()
+    {
+        $this->app['config']->set('varbox.varbox-activity.old_threshold', null);
+
+        $this->assertEquals(4, Activity::count());
+
+        $this->artisan('varbox:clean-activity');
+
+        $this->assertEquals(4, Activity::count());
+    }
+
+    /** @test */
+    public function it_doesnt_delete_any_activity_logs_if_the_days_threshold_is_zero()
+    {
+        $this->app['config']->set('varbox.varbox-activity.old_threshold', 0);
+
+        $this->assertEquals(4, Activity::count());
+
+        $this->artisan('varbox:clean-activity');
+
+        $this->assertEquals(4, Activity::count());
+    }
+
+    /**
+     * Create a permission instance.
+     *
+     * @return void
+     */
+    protected function setUpTestingConditions()
+    {
+        Activity::create([
+            'event' => 'created',
+        ]);
+
+        for ($i = 1; $i <= 3; $i++) {
+            Activity::create([
+                'event' => 'created',
+                'created_at' => today()->subDays(31),
+            ]);
+        }
+    }
+}
