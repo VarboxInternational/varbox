a9349fb15a5f7eca102f778d9f92c0a8271292c3
Andrei Badea <zbiller@gmail.com>
2019-06-28 23:11:14 +0300

    created a default pagination view

---
+++ resources/views/pagination/default.blade.php
@@ -0,0 +1,44 @@
+@if ($paginator->hasPages())
+    <ul class="pagination" role="navigation">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                <span class="page-link" aria-hidden="true">Prev</span>
+            </li>
+        @else
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->appends(request()->input())->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">Prev</a>
+            </li>
+        @endif
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+            @endif
+
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                    @else
+                        <li class="page-item"><a class="page-link" href="{{ $paginator->appends(request()->input())->url($page) }}">{{ $page }}</a></li>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->appends(request()->input())->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">Next</a>
+            </li>
+        @else
+            <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                <span class="page-link" aria-hidden="true">Next</span>
+            </li>
+        @endif
+    </ul>
+@endif
