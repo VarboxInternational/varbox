a69ebc91781801f8cea12e901acbfae9572a1861
Andrei Badea <zbiller@gmail.com>
2019-06-30 17:49:46 +0300

    added trait to support having addresses

---
+++ src/Traits/HasAddresses.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Varbox\Traits;
+
+use Varbox\Models\Address;
+
+trait HasAddresses
+{
+    /**
+     * User has many addresses.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function addresses()
+    {
+        $address = config('varbox.varbox-binding.models.address_model', Address::class);
+
+        return $this->hasMany($address, 'user_id');
+    }
+}
