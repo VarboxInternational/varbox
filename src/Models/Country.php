389e1b3a4b5ed044b77d9418bc762833f5d270fb
Andrei Badea <zbiller@gmail.com>
2019-06-30 12:54:14 +0300

    added country model class

---
+++ src/Models/Country.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Varbox\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Varbox\Options\ActivityOptions;
+use Varbox\Traits\HasActivity;
+use Varbox\Traits\IsCacheable;
+use Varbox\Traits\IsFilterable;
+use Varbox\Traits\IsSortable;
+use Varbox\Contracts\CountryModelContract;
+
+class Country extends Model implements CountryModelContract
+{
+    use HasActivity;
+    use IsCacheable;
+    use IsFilterable;
+    use IsSortable;
+
+    /**
+     * The database table.
+     *
+     * @var string
+     */
+    protected $table = 'countries';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'code',
+    ];
+
+    /**
+     * Country has many states.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function states()
+    {
+        $state = config('varbox.varbox-binding.models.state_model', State::class);
+
+        return $this->hasMany($state, 'country_id');
+    }
+
+    /**
+     * Country has many cities.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function cities()
+    {
+        $city = config('varbox.varbox-binding.models.city_model', City::class);
+
+        return $this->hasMany($city,c 'country_id');
+    }
+
+    /**
+     * Sort the query alphabetically by name.
+     *
+     * @param Builder $query
+     */
+    public function scopeAlphabetically($query)
+    {
+        $query->orderBy('name', 'asc');
+    }
+
+    /**
+     * Set the options for the HasActivity trait.
+     *
+     * @return ActivityOptions
+     */
+    public function getActivityOptions()
+    {
+        return ActivityOptions::instance()
+            ->withEntityType('country')
+            ->withEntityName($this->name)
+            ->withEntityUrl(route('admin.countries.edit', $this->getKey()));
+    }
+}
