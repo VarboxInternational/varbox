300992654dd5e649cb416c151b88f15c8f7ff240
Andrei Badea <zbiller@gmail.com>
2019-07-02 09:18:58 +0300

    added search input for global search

--- resources/views/layouts/admin/partials/_header.blade.php
+++ resources/views/layouts/admin/partials/_header.blade.php
@@ -21,7 +21,15 @@
 <div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg">
+            <div class="col-lg-3 ml-auto">
+                <form class="input-icon my-3 my-lg-0">
+                    <input type="search" class="form-control header-search" placeholder="Search…" tabindex="1">
+                    <div class="input-icon-addon">
+                        <i class="fe fe-search"></i>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg order-lg-first">
                 @include('varbox::layouts.admin.partials._menu')
             </div>
         </div>
