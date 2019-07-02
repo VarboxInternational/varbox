232514d3fe1f4825594b4b7397a1bc076f62e9c9
Andrei Badea <zbiller@gmail.com>
2019-07-02 22:46:22 +0300

    fixed the model reading the traits from for the activity model test

--- tests/Integration/ActivityTest.php
+++ tests/Integration/ActivityTest.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Varbox\Models\Activity;
-use Varbox\Models\Country;
 use Varbox\Models\User;
 use Varbox\Tests\Models\Post;
 use Varbox\Traits\IsCacheable;
@@ -43,23 +42,23 @@ class ActivityTest extends TestCase
 
         $this->setUpTestingConditions();
     }
-    
+
     /** @test */
     public function it_uses_the_is_cacheable_trait()
     {
-        $this->assertArrayHasKey(IsCacheable::class, class_uses(Country::class));
+        $this->assertArrayHasKey(IsCacheable::class, class_uses(Activity::class));
     }
 
     /** @test */
     public function it_uses_the_is_filterable_trait()
     {
-        $this->assertArrayHasKey(IsFilterable::class, class_uses(Country::class));
+        $this->assertArrayHasKey(IsFilterable::class, class_uses(Activity::class));
     }
 
     /** @test */
     public function it_uses_the_is_sortable_trait()
     {
-        $this->assertArrayHasKey(IsSortable::class, class_uses(Country::class));
+        $this->assertArrayHasKey(IsSortable::class, class_uses(Activity::class));
     }
 
 
