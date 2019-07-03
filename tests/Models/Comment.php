a0ecf3ac5c20b8c30a4d0232988498cb7880d2f9
Andrei Badea <zbiller@gmail.com>
2019-07-03 10:12:22 +0300

    added additional models for testing purposes

---
+++ tests/Models/Comment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Varbox\Tests\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+     * The database table.
+     *
+     * @var string
+     */
+    protected $table = 'test_comments';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'post_id',
+        'title',
+        'content',
+        'date',
+        'active',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
+}
--- tests/Models/Post.php
+++ tests/Models/Post.php
@@ -5,10 +5,12 @@ namespace Varbox\Tests\Models;
 use Illuminate\Database\Eloquent\Model;
 use Varbox\Options\ActivityOptions;
 use Varbox\Traits\HasActivity;
+use Varbox\Traits\IsSortable;
 
 class Post extends Model
 {
     use HasActivity;
+    use IsSortable;
 
     /**
      * The database table.
@@ -23,10 +25,47 @@ class Post extends Model
      * @var array
      */
     protected $fillable = [
+        'author_id',
         'name',
         'slug',
         'content',
+        'votes',
+        'views',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(Author::class, 'author_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function review()
+    {
+        return $this->hasOne(Review::class, 'post_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    /**
+     * A post has and belongs to many tags.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'test_post_tag', 'post_id', 'tag_id');
+    }
 
     /**
      * Set the options for the HasActivity trait.
