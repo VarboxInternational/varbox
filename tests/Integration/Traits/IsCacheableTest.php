58cf333f7f079d29208e03176e16c5b5c655d4ec
Andrei Badea <zbiller@gmail.com>
2019-07-04 16:58:33 +0300

    created integration tests for the cacheable functionality using redis

--- tests/Integration/Traits/IsCacheableTest.php
+++ tests/Integration/Traits/IsCacheableTest.php
@@ -59,6 +59,180 @@ class IsCacheableTest extends TestCase
         $this->assertTrue(true);
     }
 
+    /** @test */
+    public function it_caches_duplicate_queries_using_redis()
+    {
+        $this->app['config']->set('varbox.varbox-cache.query.all.enabled', true);
+        $this->app['config']->set('varbox.varbox-cache.query.all.store', 'redis');
+
+        $this->createPostsAndComments();
+
+        DB::enableQueryLog();
+
+        $this->executePostQueries();
+        $this->assertEquals(2, count(DB::getQueryLog()));
+
+        DB::flushQueryLog();
+
+        $this->executePostQueries();
+        $this->assertEquals(0, count(DB::getQueryLog()));
+
+        DB::disableQueryLog();
+    }
+
+    /** @test */
+    public function it_removes_cached_queries_from_redis_when_creating_a_new_record()
+    {
+        $this->app['config']->set('varbox.varbox-cache.query.all.enabled', true);
+        $this->app['config']->set('varbox.varbox-cache.query.all.store', 'redis');
+
+        $this->createPostsAndComments();
+
+        DB::enableQueryLog();
+
+        $this->executePostQueries();
+
+        Post::create([
+            'name' => 'New post name',
+            'slug' => 'new-post-name',
+            'content' => 'New post content',
+        ]);
+
+        DB::flushQueryLog();
+
+        $this->executePostQueries();
+        $this->assertEquals(2, count(DB::getQueryLog()));
+
+        DB::disableQueryLog();
+    }
+
+    /** @test */
+    public function it_removes_cached_queries_from_redis_when_updating_a_new_record()
+    {
+        $this->app['config']->set('varbox.varbox-cache.query.all.enabled', true);
+        $this->app['config']->set('varbox.varbox-cache.query.all.store', 'redis');
+
+        $this->createPostsAndComments();
+
+        DB::enableQueryLog();
+
+        $this->executePostQueries();
+
+        Post::first()->update([
+            'name' => 'Updated post name',
+        ]);
+
+        DB::flushQueryLog();
+
+        $this->executePostQueries();
+        $this->assertEquals(2, count(DB::getQueryLog()));
+
+        DB::disableQueryLog();
+    }
+
+    /** @test */
+    public function it_removes_related_cached_queries_from_redis_when_creating_a_new_record()
+    {
+        $this->app['config']->set('varbox.varbox-cache.query.all.enabled', true);
+        $this->app['config']->set('varbox.varbox-cache.query.all.store', 'redis');
+
+        $this->createPostsAndComments();
+
+        DB::enableQueryLog();
+
+        $this->executePostQueries();
+        $this->executeCommentQueries();
+
+        Post::create([
+            'name' => 'New post name',
+            'slug' => 'new-post-name',
+            'content' => 'New post content',
+        ]);
+
+        DB::flushQueryLog();
+
+        $this->executePostQueries();
+        $this->executeCommentQueries();
+
+        $this->assertEquals(4, count(DB::getQueryLog()));
+
+        DB::disableQueryLog();
+    }
+
+    /** @test */
+    public function it_removes_related_cached_queries_from_redis_when_updating_a_new_record()
+    {
+        $this->app['config']->set('varbox.varbox-cache.query.all.enabled', true);
+        $this->app['config']->set('varbox.varbox-cache.query.all.store', 'redis');
+
+        $this->createPostsAndComments();
+
+        DB::enableQueryLog();
+
+        $this->executePostQueries();
+        $this->executeCommentQueries();
+
+        Post::first()->update([
+            'name' => 'Updated post name',
+        ]);
+
+        DB::flushQueryLog();
+
+        $this->executePostQueries();
+        $this->executeCommentQueries();
+
+        $this->assertEquals(4, count(DB::getQueryLog()));
+
+        DB::disableQueryLog();
+    }
+
+    /** @test */
+    public function it_can_disable_query_caching_with_redis()
+    {
+        $this->app['config']->set('varbox.varbox-cache.query.all.enabled', true);
+        $this->app['config']->set('varbox.varbox-cache.query.all.store', 'redis');
+        $this->createPostsAndComments();
+        app(QueryCacheServiceContract::class)->disableQueryCache();
+
+        DB::enableQueryLog();
+
+        $this->executePostQueries();
+        $this->assertEquals(20, count(DB::getQueryLog()));
+
+        $this->executePostQueries();
+        $this->assertEquals(40, count(DB::getQueryLog()));
+
+        DB::disableQueryLog();
+    }
+
+    /** @test */
+    public function it_can_enable_query_caching_with_redis()
+    {
+        $this->app['config']->set('varbox.varbox-cache.query.all.enabled', true);
+        $this->app['config']->set('varbox.varbox-cache.query.all.store', 'redis');
+        $this->createPostsAndComments();
+        app(QueryCacheServiceContract::class)->disableQueryCache();
+
+        DB::enableQueryLog();
+
+        $this->executePostQueries();
+        $this->assertEquals(20, count(DB::getQueryLog()));
+
+        DB::flushQueryLog();
+
+        app(QueryCacheServiceContract::class)->enableQueryCache();
+
+        $this->executePostQueries();
+        $this->assertEquals(2, count(DB::getQueryLog()));
+
+        DB::flushQueryLog();
+
+        $this->executePostQueries();
+        $this->assertEquals(0, count(DB::getQueryLog()));
+
+        DB::disableQueryLog();
+    }
+
     /**
      * @return void
      */
