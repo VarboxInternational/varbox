acfe65c68da95036304483f66f29cbae2fc02252
Andrei Badea <zbiller@gmail.com>
2019-07-05 12:05:40 +0300

    added browser tests for being able to access user addresses

--- tests/Browser/AddressesTest.php
+++ tests/Browser/AddressesTest.php
@@ -73,4 +73,37 @@ class AddressesTest extends TestCase
 
         $this->user->delete();
     }
+
+    /** @test */
+    public function an_admin_can_access_user_addresses_if_it_is_a_super_admin()
+    {
+        $this->admin->assignRoles('Super');
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visit('/admin/users/edit/' . $this->user->id)
+                ->clickLink('View User\'s Addresses')
+                ->assertPathIs('/admin/users/' . $this->user->id . '/addresses')
+                ->assertSee('Addresses');
+        });
+
+        $this->user->delete();
+    }
+
+    /** @test */
+    public function an_admin_can_access_user_addresses_if_it_has_permission()
+    {
+        $this->admin->grantPermission('users-edit');
+        $this->admin->grantPermission('addresses-list');
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visit('/admin/users/edit/' . $this->user->id)
+                ->clickLink('View User\'s Addresses')
+                ->assertPathIs('/admin/users/' . $this->user->id . '/addresses')
+                ->assertSee('Addresses');
+        });
+
+        $this->user->delete();
+    }
 }
