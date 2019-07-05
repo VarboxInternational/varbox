e5de103b7377b9794aeae0a8ae9bb939125b1925
Andrei Badea <zbiller@gmail.com>
2019-07-05 18:51:39 +0300

    added error filter class

---
+++ src/Filters/ErrorFilter.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Varbox\Filters;
+
+class ErrorFilter extends Filter
+{
+    /**
+     * Get the main where condition between entire request fields.
+     *
+     * @return string
+     */
+    public function morph()
+    {
+        return 'and';
+    }
+
+    /**
+     * Get the filters that apply to the request.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            'search' => [
+                'operator' => Filter::OPERATOR_LIKE,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'type,code,url,message',
+            ],
+            'occurrences' => [
+                'operator' => Filter::OPERATOR_BETWEEN,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'occurrences',
+            ],
+            'start_date' => [
+                'operator' => Filter::OPERATOR_DATE_GREATER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'updated_at',
+            ],
+            'end_date' => [
+                'operator' => Filter::OPERATOR_DATE_SMALLER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'updated_at',
+            ],
+        ];
+    }
+
+    /**
+     * Get the modified value of a request filter field.
+     *
+     * @return array
+     */
+    public function modifiers()
+    {
+        return [];
+    }
+}
