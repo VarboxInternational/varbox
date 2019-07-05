fddc0f264b8f8cbeaa6e2e87b76181635fc19c00
Andrei Badea <zbiller@gmail.com>
2019-07-05 22:03:03 +0300

    updated configuration config file description

--- config/varbox-config.php
+++ config/varbox-config.php
@@ -9,8 +9,7 @@ return [
     | Specify any config key from any Laravel configuration file to make it editable.
     | The format used to specify the config keys is the same as using Laravel's "config()" helper.
     |
-    | Ones specified, you will find the config key inside "Admin -> System Settings -> Configs", ready to be edited.
-    | Please note that if a config key is left empty in the Admin, the actual value of that config key will be what's specified in the corresponding Laravel config file, and NOT "null".
+    | From Admin -> System Settings -> Configs you will be able to only create/update config vaues for the keys specified here.
     |
     | Examples:
     | - specify "app.name" as part of the below array to make the "name" key from the "app.php" config file editable;
@@ -18,10 +17,8 @@ return [
     |
     | Note: for config keys supporting arrays, simply define your values in Admin, separated by a semi-colon (;).
     |
-    | Leave the array empty to have no config keys editable.
-    |
     */
-    'name' => [
+    'keys' => [
 
     ],
 
