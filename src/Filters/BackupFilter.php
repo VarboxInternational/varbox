00c7b7008bc53498e61c7d143411dd4c4605bad6
Andrei Badea <zbiller@gmail.com>
2019-07-05 18:51:19 +0300

    added backup filter class

---
+++ src/Filters/BackupFilter.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Varbox\Filters;
+
+class BackupFilter extends Filter
+{
+    /**
+     * Get the main where condition between entire request fields.
+     *
+     * @return string
+     */
+    public function morph()
+    {
+        return 'and';
+    }
+
+    /**
+     * Get the filters that apply to the request.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            'search' => [
+                'operator' => Filter::OPERATOR_LIKE,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'name',
+            ],
+            'size' => [
+                'operator' => Filter::OPERATOR_BETWEEN,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'size',
+            ],
+            'start_date' => [
+                'operator' => Filter::OPERATOR_DATE_GREATER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'date',
+            ],
+            'end_date' => [
+                'operator' => Filter::OPERATOR_DATE_SMALLER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'date',
+            ],
+        ];
+    }
+
+    /**
+     * Get the modified value of a request filter field.
+     *
+     * @return array
+     */
+    public function modifiers()
+    {
+        return [
+            'size' => function ($modified) {
+                foreach (request()->query('size') as $size) {
+                    $modified[] = $size * pow(1024, 2);
+                }
+
+                return $modified;
+            },
+        ];
+    }
+}
