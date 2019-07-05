ec9e6e6ee713b024b9ca7ace9730b8a2b4c4f144
Andrei Badea <zbiller@gmail.com>
2019-07-05 20:03:19 +0300

    added errors controller

---
+++ src/Controllers/ErrorsController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Varbox\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Varbox\Traits\CanCrud;
+use Varbox\Contracts\ErrorModelContract;
+use Varbox\Filters\ErrorFilter;
+use Varbox\Sorts\ErrorSort;
+
+class ErrorsController extends Controller
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use CanCrud;
+
+    /**
+     * @var ErrorModelContract
+     */
+    protected $model;
+
+    /**
+     * ErrorsController constructor.
+     *
+     * @param ErrorModelContract $model
+     */
+    public function __construct(ErrorModelContract $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param Request $request
+     * @param ErrorFilter $filter
+     * @param ErrorSort $sort
+     * @return \Illuminate\View\View
+     * @throws \Exception
+     */
+    public function index(Request $request, ErrorFilter $filter, ErrorSort $sort)
+    {
+        return $this->_index(function () use ($request, $filter, $sort) {
+            $this->items = $this->model
+                ->filtered($request->all(), $filter)
+                ->sorted($request->all(), $sort)
+                ->paginate(config('varbox.varbox-crud.per_page', 10));
+
+            $this->title = 'Errors';
+            $this->view = view('varbox::admin.errors.index');
+        });
+    }
+
+    /**
+     * @param ErrorModelContract $error
+     * @return \Illuminate\View\View
+     * @throws \Exception
+     */
+    public function show(ErrorModelContract $error)
+    {
+        return $this->_edit(function () use ($error) {
+            $this->item = $error;
+            $this->title = 'View Error';
+            $this->view = view('varbox::admin.errors.show');
+        });
+    }
+
+    /**
+     * @param ErrorModelContract $error
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
+     */
+    public function destroy(ErrorModelContract $error)
+    {
+        return $this->_destroy(function () use ($error) {
+            $this->item = $error;
+            $this->redirect = redirect()->route('admin.errors.index');
+
+            $this->item->delete();
+        });
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
+     */
+    public function clear()
+    {
+        try {
+            $this->model->truncate();
+
+            flash()->success('All errors have been successfully deleted!');
+        } catch (\Exception $e) {
+            flash()->error('Could not delete the errors! Please try again.', $e);
+        }
+
+        return redirect()->route('admin.errors.index');
+    }
+}
