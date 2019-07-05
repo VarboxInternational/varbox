b35de7f33f226aee612ead9680c0ffa5aaa149b6
Andrei Badea <zbiller@gmail.com>
2019-07-05 12:02:25 +0300

    added browser tests for viewing the addresses button on a user's edit page

---
+++ tests/Browser/AddressesTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Varbox\Tests\Browser;
+
+use Varbox\Models\Address;
+use Varbox\Models\City;
+use Varbox\Models\Country;
+use Varbox\Models\State;
+use Varbox\Models\User;
+
+class AddressesTest extends TestCase
+{
+    /**
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::create([
+            'email' => 'test-user@mail.com',
+            'password' => bcrypt('test_password'),
+        ]);
+    }
+
+    /** @test */
+    public function an_admin_can_see_the_addresses_button_if_it_is_a_super_admin()
+    {
+        $this->admin->assignRoles('Super');
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visit('/admin/users/edit/' . $this->user->id)
+                ->assertSee('View User\'s Addresses');
+        });
+
+        $this->user->delete();
+    }
+
+    /** @test */
+    public function an_admin_can_see_the_addresses_button_if_it_has_permission()
+    {
+        $this->admin->grantPermission('users-edit');
+        $this->admin->grantPermission('addresses-list');
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visit('/admin/users/edit/' . $this->user->id)
+                ->assertSee('View User\'s Addresses');
+        });
+
+        $this->user->delete();
+    }
+
+    /** @test */
+    public function an_admin_cannot_see_the_addresses_button_if_it_doesnt_have_permission()
+    {
+        $this->admin->grantPermission('users-edit');
+        $this->admin->revokePermission('addresses-list');
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visit('/admin/users/edit/' . $this->user->id)
+                ->assertDontSee('View User\'s Addresses');
+        });
+
+        $this->user->delete();
+    }
+}
