7a304f1d196231dd807c2326d0ac778656de05df
Andrei Badea <zbiller@gmail.com>
2019-07-07 21:30:48 +0300

    created integration tests for the config model

--- tests/Integration/Models/ConfigTest.php
+++ tests/Integration/Models/ConfigTest.php
@@ -2,7 +2,6 @@
 
 namespace Varbox\Tests\Integration\Models;
 
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Varbox\Models\Config;
 use Varbox\Tests\Integration\TestCase;
@@ -16,44 +15,37 @@ class ConfigTest extends TestCase
      */
     protected $config;
 
-    /**
-     * @var array
-     */
-    protected $keys = [];
-
-    /**
-     * @return void
-     */
-    public function setUp(): void
+    /** @test */
+    public function it_can_return_the_value()
     {
-        parent::setUp();
-
-        $this->afterApplicationCreated(function () {
-            $this->keys = Config::getAllowedKeys();
-        });
-    }
-
-    /**
-     * @param Application $app
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        parent::getEnvironmentSetUp($app);
-
-        $app['config']->set('varbox.varbox-config.keys', ['app.name']);
-    }
-
-    
-
-    /**
-     * @return void
-     */
-    protected function createConfig()
-    {
-        $this->config = Country::create([
-            'name' => 'Test Country Name',
-            'code' => 'TCN',
+        $this->config = Config::create([
+            'key' => 'app.name',
+            'value' => 'Test App Name',
         ]);
+
+        $this->assertEquals('Test App Name', $this->config->value);
+
+        $this->config->update([
+            'value' => 'First Test App Name;Second Test App Name'
+        ]);
+
+        $this->assertIsArray($this->config->value);
+        $this->assertEquals('First Test App Name', $this->config->value[0]);
+        $this->assertEquals('Second Test App Name', $this->config->value[1]);
+    }
+
+    /** @test */
+    public function it_can_return_all_the_allowed_keys()
+    {
+        $this->app['config']->set('varbox.varbox-config.keys', [
+            'app.name', 'auth.guards.default'
+        ]);
+
+        $keys = Config::getAllowedKeys();
+
+        $this->assertIsArray($keys);
+        $this->assertCount(2, $keys);
+        $this->assertArrayHasKey('app.name', $keys);
+        $this->assertArrayHasKey('auth.guards.default', $keys);
     }
 }
