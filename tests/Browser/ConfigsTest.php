aae43ec0a74a697070b25d9e002444782f7962fe
Andrei Badea <zbiller@gmail.com>
2019-07-07 20:48:23 +0300

    created browser tests for configs edit page

--- tests/Browser/ConfigsTest.php
+++ tests/Browser/ConfigsTest.php
@@ -12,6 +12,11 @@ class ConfigsTest extends TestCase
     protected $configModel;
 
     /**
+     * @var array
+     */
+    protected $configKeys;
+
+    /**
      * @var string
      */
     protected $configKey = 'app.name';
@@ -21,6 +26,20 @@ class ConfigsTest extends TestCase
      * @var string
      */
     protected $configValueModified = 'Test App Name Modified';
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->afterApplicationCreated(function () {
+            $this->app['config']->set('varbox.varbox-config.keys', ['app.name']);
+
+            $this->configKeys = Config::getAllowedKeys();
+        });
+    }
 
     /** @test */
     public function an_admin_can_view_the_list_page_if_it_is_a_super_admin()
@@ -105,6 +124,62 @@ class ConfigsTest extends TestCase
         });
     }
 
+    /** @test */
+    public function an_admin_can_view_the_edit_page_if_it_is_a_super_admin()
+    {
+        $this->admin->assignRoles('Super');
+
+        $this->createConfig();
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visitLastPage('/admin/configs', $this->configModel)
+                ->clickEditButton($this->configKeys[$this->configKey])
+                ->assertPathIs('/admin/configs/edit/' . $this->configModel->id)
+                ->assertSee('Edit Config');
+        });
+
+        $this->deleteConfig();
+    }
+
+    /** @test */
+    public function an_admin_can_view_the_edit_page_if_it_has_permission()
+    {
+        $this->admin->grantPermission('configs-list');
+        $this->admin->grantPermission('configs-edit');
+
+        $this->createConfig();
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visitLastPage('/admin/configs', $this->configModel)
+                ->clickEditButton($this->configKeys[$this->configKey])
+                ->assertPathIs('/admin/configs/edit/' . $this->configModel->id)
+                ->assertSee('Edit Config');
+        });
+
+        $this->deleteConfig();
+    }
+
+    /** @test */
+    public function an_admin_cannot_view_the_edit_page_if_it_doesnt_have_permission()
+    {
+        $this->admin->grantPermission('configs-list');
+        $this->admin->revokePermission('configs-edit');
+
+        $this->createConfig();
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visitLastPage('/admin/configs', $this->configModel)
+                ->clickEditButton($this->configKeys[$this->configKey])
+                ->assertSee('Unauthorized')
+                ->assertDontSee('Edit Config');
+        });
+
+        $this->deleteConfig();
+    }
+
     /**
      * @return void
      */
@@ -112,7 +187,7 @@ class ConfigsTest extends TestCase
     {
         $this->configModel = Config::create([
             'key' => $this->configKey,
-            'code' => $this->configValue,
+            'value' => $this->configValue,
         ]);
     }
 
@@ -121,14 +196,6 @@ class ConfigsTest extends TestCase
      */
     protected function deleteConfig()
     {
-        Config::whereValue($this->configValue)->first()->delete();
-    }
-
-    /**
-     * @return void
-     */
-    protected function deleteCountryModified()
-    {
-        Config::whereValue($this->configValueModified)->first()->delete();
+        Config::where('key', $this->configKey)->first()->delete();
     }
 }
