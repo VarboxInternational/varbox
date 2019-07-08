4dcd92b450dc7520fde1e6c0b8f2f4c6cd5589a8
Andrei Badea <zbiller@gmail.com>
2019-07-08 22:12:50 +0300

    changed route name for creating a backup

--- routes/sys.php
+++ routes/sys.php
@@ -51,7 +51,7 @@ Route::group([
         'prefix' => 'backups',
     ], function () use ($controllers) {
         Route::get('/', ['as' => 'admin.backups.index', 'uses' => $controllers['backups'] . '@index', 'permissions' => 'backups-list']);
-        Route::post('store', ['as' => 'admin.backups.store', 'uses' => $controllers['backups'] . '@create', 'permissions' => 'backups-create']);
+        Route::post('store', ['as' => 'admin.backups.store', 'uses' => $controllers['backups'] . '@store', 'permissions' => 'backups-create']);
         Route::get('download/{backup}', ['as' => 'admin.backups.download', 'uses' => $controllers['backups'] . '@download', 'permissions' => 'backups-download']);
         Route::delete('destroy/{backup}', ['as' => 'admin.backups.destroy', 'uses' => $controllers['backups'] . '@destroy', 'permissions' => 'backups-delete']);
         Route::delete('delete-all', ['as' => 'admin.backups.delete_all', 'uses' => $controllers['backups'] . '@deleteAll', 'permissions' => 'backups-delete']);
