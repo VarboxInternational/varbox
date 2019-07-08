5184885709bf37520e416d1f337cd24f7cd75ec1
Andrei Badea <zbiller@gmail.com>
2019-07-08 23:05:33 +0300

    added action buttons for backups

--- resources/views/admin/backups/_table.blade.php
+++ resources/views/admin/backups/_table.blade.php
@@ -1,7 +1,7 @@
 <div class="card">
     <table class="table card-table table-vcenter">
         <tr>
-            <th class="sortable d-none d-sm-table-cell" data-sort="name">
+            <th class="sortable" data-sort="name">
                 <i class="fa fa-sort mr-2"></i>Name
             </th>
             <th class="sortable d-none d-sm-table-cell" data-sort="size">
@@ -17,7 +17,7 @@
                 <td>{!! $item->name ?: 'N/A' !!}</td>
                 <td class="d-none d-sm-table-cell">
                     <span class="badge badge badge-default" style="font-size: 90%;">
-                        {{ $item->size_in_mb ?: 0 }}
+                        {{ $item->size_in_mb ?: 0 }} MB
                     </span>
                 </td>
                 <td class="d-none d-sm-table-cell">
@@ -25,7 +25,8 @@
                     <div class="text-muted">{{ Carbon\Carbon::parse($item->created_at)->diffForHumans()}}</div>
                 </td>
                 <td class="text-right d-table-cell">
-                    {!! button()->deleteRecord(route('admin.activity.destroy', $item->getKey())) !!}
+                    {!! button()->downloadFile(route('admin.backups.download', $item->getKey())) !!}
+                    {!! button()->deleteRecord(route('admin.backups.destroy', $item->getKey())) !!}
                 </td>
             </tr>
         @empty
