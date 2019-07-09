2905afb13f1519a33d7fb2484ef46c4a6ed9c35d
Andrei Badea <zbiller@gmail.com>
2019-07-09 23:08:59 +0300

    added config for errors

---
+++ config/errors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |
+    | Flag indicating whether or not errors should be logged throughout the app.
+    | If set to false, no errors will be saved to the database.
+    |
+    */
+    'enabled' => env('LOG_ERRORS', true),
+
+    /*
+    |
+    | This option accepts an integer, representing the number of days.
+    |
+    | This option is used to delete error records older than the number of days supplied when:
+    | - executing the cli command: "php artisan varbox:clean-errors"
+    | - clicking the "Delete Old Errors" button from the admin panel, inside the error list view
+    |
+    | If set to "null" or "0", no past errors will be deleted whatsoever.
+    |
+    */
+    'old_threshold' => 1,
+
+];
