29509a90631201976e6954a7e2e32e9701d1f7eb
Andrei Badea <zbiller@gmail.com>
2019-07-09 22:03:03 +0300

    created http test for not allowing authenticated users

---
+++ tests/Integration/Middleware/NotAuthenticatedTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Varbox\Tests\Integration\Models;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Route;
+use Varbox\Exceptions\AuthenticationException;
+use Varbox\Models\User;
+use Varbox\Tests\Integration\TestCase;
+
+class NotAuthenticatedTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @var User
+     */
+    protected $user;
+
+    /** @test */
+    public function it_allows_unauthenticated_users()
+    {
+        $this->withoutExceptionHandling();
+
+        Route::middleware('varbox.not.authenticated')->get('/_test/not-authenticated', function () {
+            return 'OK';
+        });
+
+        $response = $this->get('/_test/not-authenticated');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('OK', $response->getContent());
+    }
+
+    /** @test */
+    public function it_doesnt_allow_admins()
+    {
+        $this->withoutExceptionHandling();
+        $this->followingRedirects();
+        $this->createUser();
+
+        Route::get('admin', function () {
+            return 'Admin';
+        });
+
+        Route::middleware('varbox.not.authenticated:admin')->get('/_test/not-authenticated', function () {
+            return 'OK';
+        });
+
+        $response = $this->actingAs($this->user, 'admin')->get('/_test/not-authenticated');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('Admin', $response->getContent());
+    }
+
+    /** @test */
+    public function it_doesnt_allow_users()
+    {
+        $this->withoutExceptionHandling();
+        $this->followingRedirects();
+        $this->createUser();
+
+        Route::get('/', function () {
+            return 'Home';
+        });
+
+        Route::middleware('varbox.not.authenticated')->get('/_test/not-authenticated', function () {
+            return 'OK';
+        });
+
+        $response = $this->actingAs($this->user, 'admin')->get('/_test/not-authenticated');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('Home', $response->getContent());
+    }
+
+    /**
+     * @return void
+     */
+    protected function createUser()
+    {
+        $this->user = User::create([
+            'email' => 'test-user@mail.com',
+            'password' => bcrypt('test_password'),
+        ]);
+    }
+}
