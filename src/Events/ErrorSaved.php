1c6a5a1f91398883dcb73d5530cf33aa73dde8ab
Andrei Badea <zbiller@gmail.com>
2019-07-10 19:04:32 +0300

    added event for saving errors

---
+++ src/Events/ErrorSaved.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Varbox\Events;
+
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Varbox\Contracts\ErrorModelContract;
+
+class ErrorSaved
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var ErrorModelContract
+     */
+    public $error;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param ErrorModelContract $error
+     * @return void
+     */
+    public function __construct(ErrorModelContract $error)
+    {
+        $this->error = $error;
+    }
+}
