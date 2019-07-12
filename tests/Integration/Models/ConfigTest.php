f2fe6a78ee2252c3858cdd57f0781f42b8ab2147
Andrei Badea <zbiller@gmail.com>
2019-07-12 13:02:37 +0300

    added trait usage integration tests for the config model

--- tests/Integration/Models/ConfigTest.php
+++ tests/Integration/Models/ConfigTest.php
@@ -4,7 +4,12 @@ namespace Varbox\Tests\Integration\Models;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Varbox\Models\Config;
+use Varbox\Models\Permission;
 use Varbox\Tests\Integration\TestCase;
+use Varbox\Traits\HasActivity;
+use Varbox\Traits\IsCacheable;
+use Varbox\Traits\IsFilterable;
+use Varbox\Traits\IsSortable;
 
 class ConfigTest extends TestCase
 {
@@ -14,6 +19,30 @@ class ConfigTest extends TestCase
      * @var Config
      */
     protected $config;
+
+    /** @test */
+    public function it_uses_the_has_activity_trait()
+    {
+        $this->assertArrayHasKey(HasActivity::class, class_uses(Config::class));
+    }
+
+    /** @test */
+    public function it_uses_the_is_cacheable_trait()
+    {
+        $this->assertArrayHasKey(IsCacheable::class, class_uses(Config::class));
+    }
+
+    /** @test */
+    public function it_uses_the_is_filterable_trait()
+    {
+        $this->assertArrayHasKey(IsFilterable::class, class_uses(Config::class));
+    }
+
+    /** @test */
+    public function it_uses_the_is_sortable_trait()
+    {
+        $this->assertArrayHasKey(IsSortable::class, class_uses(Config::class));
+    }
 
     /** @test */
     public function it_can_return_the_value()
