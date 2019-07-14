04cc549b4d51443ee08d646c0022e59bf6cbb4f4
Andrei Badea <zbiller@gmail.com>
2019-07-14 20:21:55 +0300

    added command to create symlink for uploads from storage to public

---
+++ src/Commands/UploadsLinkCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Varbox\Media\Commands;
+
+use Illuminate\Console\Command;
+
+class UploadsLinkCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'varbox:uploads-link';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a symbolic link for the uploads storage disk from "public/uploads/" to "storage/uploads/"';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function handle()
+    {
+        if (file_exists(public_path('uploads'))) {
+            $this->error('The "public/uploads/" directory already exists.');
+
+            return false;
+        }
+
+        $this->laravel->make('files')->link(
+            storage_path('uploads'), public_path('uploads')
+        );
+
+        $this->info('The "public/uploads/" directory has been linked.');
+
+        return true;
+    }
+}
