24d0c8cb273469dc7cec049c1cc3bb63ddc35464
Andrei Badea <zbiller@gmail.com>
2019-07-14 20:25:46 +0300

    added upload request validation class

---
+++ src/Requests/UploadRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Varbox\Media\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Varbox\Models\Upload;
+
+class UploadRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if (config('varbox.upload.database.save', true) !== true) {
+            return [];
+        }
+
+        return [
+            'name' => [
+                'required',
+            ],
+            'original_name' => [
+                'required',
+            ],
+            'path' => [
+                'required',
+            ],
+            'full_path' => [
+                'required',
+                Rule::unique('uploads', 'name')
+                    ->ignore($this->route('upload') ? $this->route('upload')->id : null)
+            ],
+            'extension' => [
+                'required',
+            ],
+            'size' => [
+                'required',
+                'numeric',
+            ],
+            'mime' => [
+                'required',
+            ],
+            'type' => [
+                'required',
+                'numeric',
+                Rule::in(array_keys(Upload::getFileTypes())),
+            ],
+        ];
+    }
+}
