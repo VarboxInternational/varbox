d29dce5d4a338894fef9e7f8ad83aed517f8c4db
Andrei Badea <zbiller@gmail.com>
2019-07-14 20:19:23 +0300

    added uploaded helper contract

---
+++ src/Contracts/UploadedHelperContract.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Varbox\Contracts;
+
+interface UploadedHelperContract
+{
+    /**
+     * @param string $file
+     */
+    public function __construct($file);
+
+    /**
+     * @param int|null $number
+     * @return string
+     */
+    public function thumbnail($number = null);
+
+    /**
+     * @param string|null $style
+     * @return string
+     */
+    public function url($style = null);
+
+    /**
+     * @param string|null $style
+     * @param bool $full
+     * @return string
+     */
+    public function path($style = null, $full = false);
+
+    /**
+     * @return UploadModelContract|null
+     */
+    public function load();
+
+    /**
+     * @return bool
+     */
+    public function exists();
+}
