c6c130bb32f0a264e34f378098f671ac8c788cbc
Andrei Badea <zbiller@gmail.com>
2019-07-14 20:18:41 +0300

    added upload model contract

---
+++ src/Contracts/UploadModelContract.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Varbox\Contracts;
+
+use Illuminate\Database\Schema\Blueprint;
+
+interface UploadModelContract
+{
+    /**
+     * @return string
+     */
+    public function getHelperAttribute();
+
+    /**
+     * @return string
+     */
+    public function getSizeMbAttribute();
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $types
+     */
+    public function scopeOnlyTypes($query, ...$types);
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $types
+     */
+    public function scopeExcludingTypes($query, ...$types);
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $extensions
+     */
+    public function scopeWithExtensions($query, ...$extensions);
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $extensions
+     */
+    public function scopeWithoutExtensions($query, ...$extensions);
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $mimes
+     */
+    public function scopeWithMimes($query, ...$mimes);
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $mimes
+     */
+    public function scopeWithoutMimes($query, ...$mimes);
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param int $minSize
+     * @param int $maxSize
+     */
+    public function scopeSizeBetween($query, $minSize, $maxSize);
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param array $attributes
+     */
+    public function scopeLike($query, array $attributes = []);
+
+    /**
+     * @return bool
+     */
+    public function isImage();
+
+    /**
+     * @return bool
+     */
+    public function isVideo();
+
+    /**
+     * @return bool
+     */
+    public function isAudio();
+
+    /**
+     * @return bool
+     */
+    public function isFile();
+
+    /**
+     * @return array
+     */
+    public static function getFileTypes();
+
+    /**
+     * @param string $name
+     * @param \Illuminate\Database\Schema\Blueprint $table
+     */
+    public static function column($name, Blueprint $table);
+}
