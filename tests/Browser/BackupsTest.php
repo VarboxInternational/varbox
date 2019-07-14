76fcd24d80d36feccda7a002d0ff446156d339f9
Andrei Badea <zbiller@gmail.com>
2019-07-14 15:38:52 +0300

    created browser tests for deleting backups

--- tests/Browser/BackupsTest.php
+++ tests/Browser/BackupsTest.php
@@ -2,8 +2,31 @@
 
 namespace Varbox\Tests\Browser;
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
+use Spatie\Backup\Notifications\Notifiable;
+use Varbox\Models\Backup;
+
 class BackupsTest extends TestCase
 {
+    /**
+     * @var Backup
+     */
+    protected $backupModel;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app['config']->set('backup.notifications.notifiable', Notifiable::class);
+        $this->app['config']->set('varbox.backup.name', 'VarBox');
+    }
+
     /** @test */
     public function an_admin_can_view_the_list_page_if_it_is_a_super_admin()
     {
@@ -41,5 +64,104 @@ class BackupsTest extends TestCase
                 ->assertSee('Unauthorized')
                 ->assertDontSee('Backups');
         });
+    }
+
+    /** @test */
+    public function an_admin_can_delete_a_backup_if_it_a_super_admin()
+    {
+        $this->admin->assignRoles('Super');
+
+        $this->createBackup();
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visitLastPage('/admin/backups/', $this->backupModel)
+                ->assertSee($this->backupModel->name)
+                ->assertSee($this->backupModel->size_in_mb)
+                ->assertSee($this->backupModel->date->toDateTimeString())
+                ->deleteRecord($this->backupModel->name)
+                ->assertSee('The record was successfully deleted!')
+                ->visitLastPage('/admin/backups/', $this->backupModel)
+                ->assertDontSee($this->backupModel->name)
+                ->assertDontSee($this->backupModel->size_in_mb)
+                ->assertDontSee($this->backupModel->date->toDateTimeString());
+        });
+
+        $this->assertFileNotExists($this->backupPath());
+    }
+
+    /** @test */
+    public function an_admin_can_delete_a_backup_if_it_has_permission()
+    {
+        $this->admin->grantPermission('backups-list');
+        $this->admin->grantPermission('backups-delete');
+
+        $this->createBackup();
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visitLastPage('/admin/backups/', $this->backupModel)
+                ->assertSee($this->backupModel->name)
+                ->assertSee($this->backupModel->size_in_mb)
+                ->assertSee($this->backupModel->date->toDateTimeString())
+                ->deleteRecord($this->backupModel->name)
+                ->assertSee('The record was successfully deleted!')
+                ->visitLastPage('/admin/backups/', $this->backupModel)
+                ->assertDontSee($this->backupModel->name)
+                ->assertDontSee($this->backupModel->size_in_mb)
+                ->assertDontSee($this->backupModel->date->toDateTimeString());
+        });
+
+        $this->assertFileNotExists($this->backupPath());
+    }
+
+    /** @test */
+    public function an_admin_cannot_delete_a_backup_if_it_doesnt_have_permission()
+    {
+        $this->admin->grantPermission('backups-list');
+        $this->admin->revokePermission('backups-delete');
+
+        $this->createBackup();
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visit('/admin/backups')
+                ->deleteAnyRecord()
+                ->assertDontSee('The record was successfully deleted!')
+                ->assertSee('Unauthorized');
+        });
+
+        $this->cleanBackups();
+    }
+
+    /**
+     * @return void
+     */
+    protected function createBackup()
+    {
+        Artisan::call('backup:run');
+
+        $this->backupModel = Backup::first();
+    }
+
+    /**
+     * @return void
+     */
+    protected function cleanBackups()
+    {
+        Storage::disk($this->backupModel->disk)->delete(
+            Storage::disk($this->backupModel->disk)->allFiles(config('varbox.backup.name'))
+        );
+
+        Backup::truncate();
+    }
+
+    /**
+     * @return string
+     */
+    protected function backupPath()
+    {
+        return Storage::disk($this->backupModel->disk)->getDriver()->getAdapter()->getPathPrefix() .
+            $this->backupModel->path;
     }
 }
