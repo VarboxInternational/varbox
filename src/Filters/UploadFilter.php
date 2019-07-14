b71475bd97ee4559fe40b584b596a67049a7ceca
Andrei Badea <zbiller@gmail.com>
2019-07-14 20:24:38 +0300

    added upload filter class

---
+++ src/Filters/UploadFilter.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Varbox\Filters;
+
+class UploadFilter extends Filter
+{
+    /**
+     * Get the main where condition between entire request fields.
+     *
+     * @return string
+     */
+    public function morph()
+    {
+        return 'and';
+    }
+
+    /**
+     * Get the filters that apply to the request.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            'search' => [
+                'operator' => Filter::OPERATOR_LIKE,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'original_name,full_path',
+            ],
+            'type' => [
+                'operator' => Filter::OPERATOR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'type',
+            ],
+            'size' => [
+                'operator' => Filter::OPERATOR_BETWEEN,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'size',
+            ],
+            'start_date' => [
+                'operator' => Filter::OPERATOR_DATE_GREATER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'date',
+            ],
+            'end_date' => [
+                'operator' => Filter::OPERATOR_DATE_SMALLER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'date',
+            ],
+        ];
+    }
+
+    /**
+     * Get the modified value of a request filter field.
+     *
+     * @return array
+     */
+    public function modifiers()
+    {
+        return [
+            'size' => function ($modified) {
+                foreach (request()->query('size') as $size) {
+                    $modified[] = $size * pow(1024, 2);
+                }
+
+                return $modified;
+            },
+        ];
+    }
+}
