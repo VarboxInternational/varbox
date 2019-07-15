9c91f9e0030711c94a0ee0603fb85aeff9c24b51
Andrei Badea <zbiller@gmail.com>
2019-07-15 22:29:51 +0300

    updated the download button color

--- resources/views/helpers/button/download_file.blade.php
+++ resources/views/helpers/button/download_file.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ $url }}" class="button-download d-inline btn icon px-0 mr-2" data-toggle="tooltip" data-placement="top" title="Download" {!! implode(' ', $attributes) !!}>
-    <i class="fe fe-download text-blue"></i>
+    <i class="fe fe-download text-green"></i>
 </a>
