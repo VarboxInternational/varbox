ba76b2b6d2249ceb0ac49b7c0b08a2a7cec3392f
Andrei Badea <zbiller@gmail.com>
2019-07-21 16:22:52 +0300

    updated the blade view for new uploads

--- resources/views/helpers/uploader/partials/new.blade.php
+++ resources/views/helpers/uploader/partials/new.blade.php
@@ -1,11 +1,72 @@
 @php($canSelect = auth()->user()->isSuper() || auth()->user()->hasPermission('uploads-select'))
 
-<a id="open-upload-new-{!! $index !!}" data-popup="open" data-popup-id="upload-new-{!! $index !!}" class="open-upload-new btn gray centered bordered left no-margin visible-text {!! $current ? 'half' : 'full' !!} {!! $disabled || !$canSelect ? 'disabled' : '' !!}">
-    Choose Upload
+<a class="open-upload-new btn btn-square btn-white float-left font-weight-normal @if($current) w-50 border-right-0 @else w-100 @endif" @if(!$canSelect) disabled="disabled" @endif
+    id="open-upload-new-{!! $index !!}"
+    data-toggle="modal" data-target="#upload-new-{!! $index !!}"
+    style="border: 1px solid rgba(0,40,100,.12)">
+    Choose File
 </a>
 
 @if($disabled === false)
-    <section id="upload-new-{!! $index !!}" class="upload-new popup" data-model="{{ get_class($model) }}" data-field="{{ $field }}">
+    <div class="modal fade upload-new" id="upload-new-{!! $index !!}" data-model="{{ get_class($model) }}" data-field="{{ $field }}" data-index="{{ $index }}" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg" style="overflow: initial;">
+            <div class="modal-content">
+                <div class="modal-header pb-0">
+
+                    <ul class="nav nav-tabs no-border" id="upload-tab-{{ $index }}" role="tablist">
+                        @foreach($types as $type)
+                            <li class="nav-item px-0">
+                                <a class="btn-upload-switch nav-link px-4 pt-0 @if($loop->first) active @endif" id="{!! $type !!}-tab-{{ $index }}" data-type="{{ $type }}" data-toggle="tab" href="#{!! $type !!}-{{ $index }}" role="tab" data-accept="{{ $accept && is_array($accept) && !empty($accept) ? json_encode($accept) : null }}">
+                                    {{ Str::title(str_replace('_', ' ', $type)) }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="tab-content" id="upload-tab-content-{{ $index }}">
+                        @foreach($types as $type)
+                            <div class="tab-pane fade @if($loop->first) show active @endif" id="{!! $type !!}-{{ $index }}" role="tabpanel" aria-labelledby="{!! $type !!}-tab-{{ $index }}">
+                                <input type="search" placeholder="Search for {!! $type !!}" class="form-control px-2 border-left-0 border-right-0 border-top-0" style="border-radius: 0px;" />
+                                <div class="modal-items mt-3" style="height: 325px; overflow-y: auto;">
+                                    <p class="px-2 text-muted-dark">No {{ Str::plural($type) }} found</p>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    @permission('uploads-upload')
+                        <label class="upload-btn btn btn-green btn-square text-white">
+                            <i class="fe fe-upload mr-2"></i>Upload New
+                            <input type="file" name="file" accept="{!! $accept && is_array($accept) && !empty($accept) ? '.' . implode(',.', $accept) : '*' !!}" hidden>
+                        </label>
+                    @endpermission
+
+                    <p class="upload-message pt-1 font-weight-bold"></p>
+
+                    @permission('uploads-select')
+                        <a id="upload-save-{!! $index !!}" class="btn-upload-save btn btn-blue btn-square text-white ml-auto">
+                            <i class="fe fe-check"></i>&nbsp; Save
+                        </a>
+                    @endpermission
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+
+
+
+
+
+
+
+
+    {{--<section id="upload-new-{!! $index !!}" class="upload-new popup" data-model="{{ get_class($model) }}" data-field="{{ $field }}">
         <div class="modal">
             <div class="loading">
                 <img src="{{ asset('/vendor/varbox/images/loading.gif') }}" />
@@ -53,5 +114,4 @@
                 </div>
             </div>
         </div>
-    </section>
-@endif
+    </section>--}}
