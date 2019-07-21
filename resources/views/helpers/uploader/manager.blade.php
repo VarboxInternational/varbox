b6d5231f11858780d6a201589048a4a390f01998
Andrei Badea <zbiller@gmail.com>
2019-07-21 16:35:58 +0300

    updated the blade view for managing uploads via the uploader

--- resources/views/helpers/uploader/manager.blade.php
+++ resources/views/helpers/uploader/manager.blade.php
@@ -1,11 +1,13 @@
-<fieldset>
-    <label>{!! $label !!}</label>
-    <div class="field-wrapper">
+<div class="form-group">
+    <label class="form-label">{!! $label !!}<span class="form-required">*</span></label>
+
+    <div class="d-flex w-100 ">
         @include('varbox::helpers.uploader.partials.new')
         @include('varbox::helpers.uploader.partials.current')
     </div>
+
     {!! form()->hidden($field, $current ? $current->path('original') : null, ['id' => 'upload-input-' . $index, 'class' => 'upload-input']) !!}
-</fieldset>
+</div>
 
 @if($i == 0)
     @include('varbox::helpers.uploader.partials.scripts')
