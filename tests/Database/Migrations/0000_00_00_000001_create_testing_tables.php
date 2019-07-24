d9c683f9e87ec29a5d9d5ccb2b3cb6f17a01331a
Andrei Badea <zbiller@gmail.com>
2019-07-24 22:45:04 +0300

    added extra fields inside testing migration file

--- tests/Database/Migrations/0000_00_00_000001_create_testing_tables.php
+++ tests/Database/Migrations/0000_00_00_000001_create_testing_tables.php
@@ -35,6 +35,7 @@ class CreateTestingTables extends Migration
             $table->text('content')->nullable();
             $table->integer('votes')->default(0);
             $table->integer('views')->default(0);
+            $table->boolean('approved')->default(false);
             $table->timestamp('published_at')->nullable();
             $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade')->onUpdate('set null');
             $table->timestamps();
@@ -57,6 +58,7 @@ class CreateTestingTables extends Migration
             $table->text('content')->nullable();
             $table->date('date')->nullable();
             $table->boolean('active')->default(false);
+            $table->integer('votes')->default(0);
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
