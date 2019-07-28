399c45eb4e152149a3d64cf06a43c634b8adeebe
Andrei Badea <zbiller@gmail.com>
2019-07-28 18:37:31 +0300

    removed action & rollback buttons

--- resources/views/helpers/button/action.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="{{ $url ?? '#' }}" class="btn {!! $class ?? '' !!}" {!! implode(' ', $attributes) !!}>
-    <i class="fa {!! $icon ?? '' !!}"></i>&nbsp; {!! $text ?? 'Click' !!}
-</a>
--- resources/views/helpers/button/rollback_revision.blade.php
+++
@@ -1,3 +0,0 @@
-{!! form()->open(['url' => $url, 'method' => 'POST', 'class' => 'left']) !!}
-{!! form()->button('<i class="fa fa-undo"></i>&nbsp; Rollback', ['type' => 'submit', 'class' => 'btn-rollback-revision btn green', 'onclick' => 'return confirm("Are you sure you want to rollback this revision?")'] + ($attributes ? (array)$attributes : [])) !!}
-{!! form()->close() !!}
