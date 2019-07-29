a3ef11302adf338ee231be2a6274966fe06a638b
Andrei Badea <zbiller@gmail.com>
2019-07-30 00:30:04 +0300

    changed icon for the go back button

--- resources/views/helpers/button/go_back.blade.php
+++ resources/views/helpers/button/go_back.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ $url }}" class="btn text-blue mr-auto" {!! implode(' ', $attributes) !!}>
-    <i class="fe fe-chevron-left mr-1"></i>Go Back
+    <i class="fe fe-arrow-left mr-1"></i>Go Back
 </a>
