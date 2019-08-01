9b55b9233ecac6cd15282f0cfd43fdd3b1ae189a
Andrei Badea <zbiller@gmail.com>
2019-08-01 23:29:00 +0300

    cleaned the emails form: removed deprecated draft & revision sections

--- resources/views/admin/emails/_form.blade.php
+++ resources/views/admin/emails/_form.blade.php
@@ -1,7 +1,7 @@
 {!! validation('admin')->errors() !!}
 
 @if($item->exists)
-    @if(isset($on_revision))
+    @if(isset($revision))
         {!! form_admin()->model($item, ['class' => 'frm row row-cards']) !!}
     @else
         {!! form_admin()->model($item, ['url' => $url, 'method' => 'PUT', 'class' => 'frm row row-cards', 'files' => true]) !!}
@@ -9,11 +9,6 @@
 @else
     {!! form_admin()->open(['url' => $url, 'method' => 'POST', 'class' => 'frm row row-cards', 'files' => true]) !!}
 @endif
-
-{!! form()->hidden('draft[model_id]', $item->exists ? $item->id : null) !!}
-{!! form()->hidden('draft[model_class]', config('varbox.bindings.models.email_model', \Varbox\Models\Email::class)) !!}
-{!! form()->hidden('draft[validation_request]', config('varbox.bindings.form_requests.email_form_request', \Varbox\Requests\EmailRequest::class)) !!}
-{!! form()->hidden('draft[redirect_route]', 'admin.emails.edit') !!}
 
 <div class="col-md-12">
     <div class="card">
@@ -75,6 +70,7 @@
         </div>
     </div>
 </div>
+
 @if($item->exists && !empty($variables))
 <div class="col-md-12">
     <div class="card">
@@ -118,68 +114,40 @@
     </div>
 </div>
 @endif
-@if($item->exists && !isset($on_revision))
-    {!! revision()->container($item, 'admin.emails.revision') !!}
+
+@if($item->exists)
+    @if(isset($revision))
+        {!! revision()->container('admin.emails.revision', $item, $revision) !!}
+    @else
+        {!! revision()->container('admin.emails.revision', $item) !!}
+        {!! draft()->container('admin.emails.publish', $item) !!}
+    @endif
 @endif
-@if($item->exists && $item->isDrafted() && !isset($on_revision))
 
-
-
-
-
-@section('top')
-    <div class="alert alert-info col-lg-12 mb-5">
-        <div class="d-inline-block float-left text-left mx-auto" style="margin-top: 2px;">
-            <i class="fe fe-info mr-2" aria-hidden="true"></i>
-        </div>
-        <div class="d-inline-block">
-            <h4>This record is currently drafted!</h4>
-            <p>
-                Please note that if you have un-saved changes,
-                you will have to save them before publishing the draft for them to be persisted.
-            </p>
-            <div class="btn-list mt-4">
-                @permission('drafts-publish')
-                {!! form()->open(['url' => route('admin.drafts.publish'), 'method' => 'PUT', 'class' => 'float-left d-inline']) !!}
-                {!! form()->hidden('_id', $item->getKey()) !!}
-                {!! form()->hidden('_class', $item->getMorphClass()) !!}
-                {!! form()->button(' <i class="fe fe-check mr-2"></i>Publish Draft', ['type' => 'submit', 'class' => 'confirm-are-you-sure btn btn-blue']) !!}
-                {!! form()->close() !!}
-                @endpermission
-            </div>
-        </div>
-    </div>
-@append
-
-
-
-
-
-
-@endif
-@if(!isset($on_revision))
-<div class="col-12">
-    <div class="card">
-        <div class="card-body">
-            <div class="d-flex text-left">
-                {!! button()->cancelAction(route('admin.emails.index')) !!}
-                @if($item->exists)
-                    {!! button()->duplicateRecord(route('admin.emails.duplicate', $item->getKey())) !!}
-                    @if(!$item->isDrafted())
-                        {!! button()->saveAsDraft(route('admin.drafts.save')) !!}
+@if(!isset($revision))
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex text-left">
+                    {!! button()->cancelAction(route('admin.emails.index')) !!}
+                    @if($item->exists)
+                        {!! button()->duplicateRecord(route('admin.emails.duplicate', $item->getKey())) !!}
+                        @if(!$item->isDrafted())
+                            {!! button()->saveAsDraft(route('admin.emails.draft', $item->getKey())) !!}
+                        @endif
+                        {!! button()->saveAndStay() !!}
+                    @else
+                        {!! button()->saveAndNew() !!}
+                        {!! button()->saveAsDraft(route('admin.emails.draft')) !!}
+                        {!! button()->saveAndContinue('admin.emails.edit') !!}
                     @endif
-                    {!! button()->saveAndStay() !!}
-                @else
-                    {!! button()->saveAndNew() !!}
-                    {!! button()->saveAsDraft(route('admin.drafts.save')) !!}
-                    {!! button()->saveAndContinue('admin.emails.edit') !!}
-                @endif
-                {!! button()->saveRecord() !!}
+                    {!! button()->saveRecord() !!}
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endif
+
 {!! form_admin()->close() !!}
 
 @push('scripts')
