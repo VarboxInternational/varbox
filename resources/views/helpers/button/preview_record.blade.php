6dc1e4431837e97a7e23d4af0f9e01123cc46ca5
Andrei Badea <zbiller@gmail.com>
2019-08-02 00:15:06 +0300

    updated the html of the preview record button helper

--- resources/views/helpers/button/preview_record.blade.php
+++ resources/views/helpers/button/preview_record.blade.php
@@ -1,3 +1,3 @@
-<a class="btn-preview-record btn yellow" data-url="{{ $url }}" {!! implode(' ', $attributes) !!}>
-    <i class="fa fa-eye"></i>&nbsp; Preview
+<a data-url="{{ $url }}" class="button-preview btn btn-dark btn-square text-white ml-4" {!! implode(' ', $attributes) !!}>
+    <i class="fe fe-eye mr-2"></i>Preview
 </a>
