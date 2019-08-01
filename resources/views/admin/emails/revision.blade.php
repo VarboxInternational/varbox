0b0075a2247a0c28219c49d5334e4992411ee164
Andrei Badea <zbiller@gmail.com>
2019-08-01 23:27:26 +0300

    removed email revision view

--- resources/views/admin/emails/revision.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('varbox::layouts.admin.default')
-
-@section('title', $title)
-
-@section('content')
-    @include('varbox::admin.emails._form', ['on_revision' => true])
-@endsection
-
-{!! revision()->view($revision, $item) !!}
