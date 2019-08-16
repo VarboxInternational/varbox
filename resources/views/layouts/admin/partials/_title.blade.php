3c0fba620bdf5a04cf7d2672fd90b12311dca232
Andrei Badea <zbiller@gmail.com>
2019-08-16 11:05:55 +0300

    added support for displaying number of results if not paginated

--- resources/views/layouts/admin/partials/_title.blade.php
+++ resources/views/layouts/admin/partials/_title.blade.php
@@ -1,18 +1,29 @@
 <h1 class="page-title float-left">@yield('title', '')</h1>
 
-@if(isset($items) && $items instanceof \Illuminate\Contracts\Pagination\LengthAwarePaginator)
-    @php($total = number_format($items->total(), 0, ',', '.'))
+@if(isset($items))
+    @if($items instanceof \Illuminate\Support\Collection)
+        @php($total = number_format($items->count(), 0, ',', '.'))
 
-    @if($total > 0)
-        @php($start = $items->currentPage() * $items->perPage() - ($items->perPage() - 1))
-        @if($items->currentPage() * $items->perPage() < $items->total())
-            @php($end = $items->currentPage() * $items->perPage())
-        @else
-            @php($end = $items->total())
+        @if($total > 0)
+            <div class="page-subtitle float-left">
+                1 - {{ $total }} of {{ $total }}
+            </div>
         @endif
+    @endif
+    @if($items instanceof \Illuminate\Contracts\Pagination\LengthAwarePaginator)
+        @php($total = number_format($items->total(), 0, ',', '.'))
 
-        <div class="page-subtitle float-left">
-            {{ $start }} - {{ $end }} of {{ $total }}
-        </div>
+        @if($total > 0)
+            @php($start = $items->currentPage() * $items->perPage() - ($items->perPage() - 1))
+            @if($items->currentPage() * $items->perPage() < $items->total())
+                @php($end = $items->currentPage() * $items->perPage())
+            @else
+                @php($end = $items->total())
+            @endif
+
+            <div class="page-subtitle float-left">
+                {{ $start }} - {{ $end }} of {{ $total }}
+            </div>
+        @endif
     @endif
 @endif
