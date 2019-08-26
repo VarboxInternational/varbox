cb8c9f269a3ad99f9e03c70c4abf1b7aea4453de
Andrei Badea <zbiller@gmail.com>
2019-08-26 17:55:42 +0300

    removed old testing tables from the testing migration file

--- tests/Database/Migrations/0000_00_00_000001_create_testing_tables.php
+++ tests/Database/Migrations/0000_00_00_000001_create_testing_tables.php
@@ -230,65 +230,6 @@ class CreateTestingTables extends Migration
             $table->string('slug')->nullable();
             $table->timestamps();
         });
-
-        Schema::create('test_authors', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title')->nullable();
-            $table->string('name');
-            $table->integer('age')->default(0);
-            $table->string('slug')->nullable();
-            $table->timestamps();
-        });
-
-        Schema::create('test_posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('author_id')->unsigned()->index()->nullable();
-            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade')->onUpdate('set null');
-            $table->string('name');
-            $table->text('content')->nullable();
-            $table->integer('votes')->default(0);
-            $table->integer('views')->default(0);
-            $table->boolean('approved')->default(false);
-            $table->timestamp('published_at')->nullable();
-            $table->timestamps();
-        });
-
-        Schema::create('test_reviews', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('post_id')->unsigned()->index();
-            $table->string('name');
-            $table->text('content')->nullable();
-            $table->integer('rating')->default(0);
-            $table->timestamps();
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-        });
-
-        Schema::create('test_comments', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('post_id')->unsigned()->index();
-            $table->string('title');
-            $table->text('content')->nullable();
-            $table->date('date')->nullable();
-            $table->boolean('active')->default(false);
-            $table->integer('votes')->default(0);
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
-            $table->timestamps();
-        });
-
-        Schema::create('test_tags', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
-        });
-
-        Schema::create('test_post_tag', function (Blueprint $table) {
-            $table->integer('post_id')->unsigned();
-            $table->integer('tag_id')->unsigned();
-            $table->timestamps();
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
-            $table->primary(['post_id', 'tag_id']);
-        });
     }
 
     /**
@@ -298,12 +239,6 @@ class CreateTestingTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('test_post_tag');
-        Schema::dropIfExists('test_tags');
-        Schema::dropIfExists('test_comments');
-        Schema::dropIfExists('test_reviews');
-        Schema::dropIfExists('test_posts');
-        Schema::dropIfExists('test_authors');
         Schema::dropIfExists('url_posts');
         Schema::dropIfExists('upload_posts');
         Schema::dropIfExists('sort_reviews');
