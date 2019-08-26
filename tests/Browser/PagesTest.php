2a69b05d7bfd6ba9360a45bf55fd46663a1ed171
Andrei Badea <zbiller@gmail.com>
2019-08-26 23:39:07 +0300

    created browser tests for viewing the edit page section

--- tests/Browser/PagesTest.php
+++ tests/Browser/PagesTest.php
@@ -17,12 +17,37 @@ class PagesTest extends TestCase
      * @var string
      */
     protected $pageName = 'Test Page Name';
+
+    /**
+     * @var string
+     */
+    protected $pageSlug = 'test-page-name';
+
+    /**
+     * @var string
+     */
     protected $pageType = 'test-type';
 
     /**
      * @var string
      */
     protected $pageNameModified = 'Test Page Name Modified';
+
+    /**
+     * @var string
+     */
+    protected $pageSlugModified = 'test-page-name-modified';
+
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        cache()->forget('first_tree_load');
+    }
 
     /**
      * @param Application $app
@@ -126,6 +151,69 @@ class PagesTest extends TestCase
         });
     }
 
+    /** @test */
+    public function an_admin_can_view_the_edit_page_if_it_is_a_super_admin()
+    {
+        $this->admin->assignRoles('Super');
+
+        $this->createPage();
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visitLastPage('/admin/pages', $this->pageModel)
+                ->waitFor('#root_id_anchor')
+                ->click('#root_id_anchor')
+                ->waitFor('.js-TreeTable')
+                ->clickEditRecordButton($this->pageName)
+                ->assertPathIs('/admin/pages/edit/' . $this->pageModel->id)
+                ->assertSee('Edit Page');
+        });
+
+        $this->deletePage();
+    }
+
+    /** @test */
+    public function an_admin_can_view_the_edit_page_if_it_has_permission()
+    {
+        $this->admin->grantPermission('pages-list');
+        $this->admin->grantPermission('pages-edit');
+
+        $this->createPage();
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visitLastPage('/admin/pages', $this->pageModel)
+                ->waitFor('#root_id_anchor')
+                ->click('#root_id_anchor')
+                ->waitFor('.js-TreeTable')
+                ->clickEditRecordButton($this->pageName)
+                ->assertPathIs('/admin/pages/edit/' . $this->pageModel->id)
+                ->assertSee('Edit Page');
+        });
+
+        $this->deletePage();
+    }
+
+    /** @test */
+    public function an_admin_cannot_view_the_edit_page_if_it_doesnt_have_permission()
+    {
+        $this->admin->grantPermission('pages-list');
+        $this->admin->revokePermission('pages-edit');
+
+        $this->createPage();
+
+        $this->browse(function ($browser) {
+            $browser->loginAs($this->admin, 'admin')
+                ->visitLastPage('/admin/pages', $this->pageModel)
+                ->assertSourceMissing('button-edit')
+                ->visit('/admin/pages/edit/' . $this->pageModel->id)
+                ->assertSee('Unauthorized')
+                ->assertDontSee('Edit Page');
+        });
+
+        $this->deletePage();
+    }
+
     /**
      * @return void
      */
@@ -133,6 +221,7 @@ class PagesTest extends TestCase
     {
         $this->pageModel = Page::create([
             'name' => $this->pageName,
+            'slug' => $this->pageSlug,
             'type' => $this->pageType,
         ]);
     }
@@ -154,6 +243,7 @@ class PagesTest extends TestCase
     {
         $this->pageModel = Page::create([
             'name' => $this->pageNameModified,
+            'slug' => $this->pageSlugModified,
             'type' => $this->pageType,
         ]);
     }
