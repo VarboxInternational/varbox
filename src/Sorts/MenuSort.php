de2efad7b7e4988bfa4a073d568d0a603ce98b99
Andrei Badea <zbiller@gmail.com>
2019-08-28 22:40:19 +0300

    added menu sort class

---
+++ src/Sorts/MenuSort.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Varbox\Sorts;
+
+class MenuSort extends Sort
+{
+    /**
+     * Get the request field name to sort by.
+     *
+     * @return string
+     */
+    public function field()
+    {
+        return 'sort';
+    }
+
+    /**
+     * Get the direction to sort by.
+     *
+     * @return string
+     */
+    public function direction()
+    {
+        return 'direction';
+    }
+}
