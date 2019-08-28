8d90678bcd46da3d6fbf92db2ca583b1a52650fd
Andrei Badea <zbiller@gmail.com>
2019-08-28 22:40:05 +0300

    added menu filter class

---
+++ src/Filters/MenuFilter.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Varbox\Filters;
+
+class MenuFilter extends Filter
+{
+    /**
+     * Get the main where condition between entire request fields.
+     *
+     * @return string
+     */
+    public function morph()
+    {
+        return 'and';
+    }
+
+    /**
+     * Get the filters that apply to the request.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            'search' => [
+                'operator' => Filter::OPERATOR_LIKE,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'name,url',
+            ],
+            'type' => [
+                'operator' => Filter::OPERATOR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'type',
+            ],
+            'active' => [
+                'operator' => Filter::OPERATOR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'active',
+            ],
+            'start_date' => [
+                'operator' => Filter::OPERATOR_DATE_GREATER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'created_at',
+            ],
+            'end_date' => [
+                'operator' => Filter::OPERATOR_DATE_SMALLER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'created_at',
+            ],
+        ];
+    }
+
+    /**
+     * Get the modified value of a request filter field.
+     *
+     * @return array
+     */
+    public function modifiers()
+    {
+        return [];
+    }
+}
