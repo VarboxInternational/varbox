0dcce4e08aa8ec80a7bd67f1f7f678ea88881848
Andrei Badea <zbiller@gmail.com>
2019-08-29 12:20:21 +0300

    updated the menu tree routes

--- routes/menus.php
+++ routes/menus.php
@@ -22,7 +22,7 @@ Route::group([
     ], function () use ($controllers) {
         Route::get('locations', ['as' => 'admin.menus.locations', 'uses' => $controllers['menus'] . '@locations', 'permissions' => 'menus-list']);
         Route::get('entity/{type?}', ['as' => 'admin.menus.entity', 'uses' => $controllers['menus'] . '@entity', 'permissions' => 'menus-list']);
-
+        Route::get('route', ['as' => 'admin.menus.route', 'uses' => $controllers['menus'] . '@route', 'permissions' => 'menus-list']);
         Route::get('{location}', ['as' => 'admin.menus.index', 'uses' => $controllers['menus'] . '@index', 'permissions' => 'menus-list']);
         Route::get('{location}/create/{menuParent?}', ['as' => 'admin.menus.create', 'uses' => $controllers['menus'] . '@create', 'permissions' => 'menus-add']);
         Route::post('{location}/store/{menuParent?}', ['as' => 'admin.menus.store', 'uses' => $controllers['menus'] . '@store', 'permissions' => 'menus-add']);
@@ -36,10 +36,10 @@ Route::group([
         Route::group([
             'prefix' => 'tree'
         ], function () use ($controllers) {
-            Route::get('fix', ['as' => 'admin.menus.tree.fix', 'uses' => $controllers['tree'] . '@fixTree', 'permissions' => 'menus-list']);
-            Route::get('{location}/load/{parent?}', ['as' => 'admin.menus.tree.load', 'uses' => $controllers['tree'] . '@loadNodes', 'permissions' => 'menus-list']);
-            Route::get('{location}/list/{parent?}', ['as' => 'admin.menus.tree.list', 'uses' => $controllers['tree'] . '@listItems', 'permissions' => 'menus-list']);
-            Route::post('sort', ['as' => 'admin.menus.tree.sort', 'uses' => $controllers['tree'] . '@sortItems', 'permissions' => 'menus-list']);
+            Route::get('{location}/load/{parent?}', ['as' => 'admin.menus.tree.load', 'uses' => $controllers['tree'] . '@loadNodes']);
+            Route::get('{location}/list/{parent?}', ['as' => 'admin.menus.tree.list', 'uses' => $controllers['tree'] . '@listItems']);
+            Route::put('fix', ['as' => 'admin.menus.tree.fix', 'uses' => $controllers['tree'] . '@fixTree']);
+            Route::post('sort', ['as' => 'admin.menus.tree.sort', 'uses' => $controllers['tree'] . '@sortItems']);
         });
     });
 });
