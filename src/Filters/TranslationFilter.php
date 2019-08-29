bfec251822839fb066483d0a5ea3a103073b18fc
Andrei Badea <zbiller@gmail.com>
2019-08-30 00:21:26 +0300

    added translation filter class

---
+++ src/Filters/TranslationFilter.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Varbox\Filters;
+
+class TranslationFilter extends Filter
+{
+    /**
+     * Get the main where condition between entire request fields.
+     *
+     * @return string
+     */
+    public function morph()
+    {
+        return 'and';
+    }
+
+    /**
+     * Get the filters that apply to the request.
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return [
+            'search' => [
+                'operator' => Filter::OPERATOR_LIKE,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'key,value',
+            ],
+            'locale' => [
+                'operator' => Filter::OPERATOR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'locale',
+            ],
+            'group' => [
+                'operator' => Filter::OPERATOR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'group',
+            ],
+            'start_date' => [
+                'operator' => Filter::OPERATOR_DATE_GREATER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'created_at',
+            ],
+            'end_date' => [
+                'operator' => Filter::OPERATOR_DATE_SMALLER_OR_EQUAL,
+                'condition' => Filter::CONDITION_OR,
+                'columns' => 'created_at',
+            ],
+        ];
+    }
+
+    /**
+     * Get the modified value of a request filter field.
+     *
+     * @return array
+     */
+    public function modifiers()
+    {
+        return [];
+    }
+}
