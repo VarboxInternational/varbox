e36c33237d18c858ee038b990d99bf97c1643fac
Andrei Badea <zbiller@gmail.com>
2019-08-31 19:28:10 +0300

    removed the breadcrumbs for creating translations

--- breadcrumbs/translations.php
+++ breadcrumbs/translations.php
@@ -6,12 +6,6 @@ Breadcrumbs::register('admin.translations.index', function($breadcrumbs) {
     $breadcrumbs->push('Translations    ', route('admin.translations.index'));
 });
 
-/* Home > Translations > Add */
-Breadcrumbs::register('admin.translations.create', function($breadcrumbs) {
-    $breadcrumbs->parent('admin.translations.index');
-    $breadcrumbs->push('Add', route('admin.translations.create'));
-});
-
 /* Home > Translations > Edit */
 Breadcrumbs::register('admin.translations.edit', function($breadcrumbs, $country) {
     $breadcrumbs->parent('admin.translations.index');
