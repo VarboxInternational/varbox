3b70902a7ee5fc915f01dfb31805ae547be40c8d
Andrei Badea <zbiller@gmail.com>
2019-08-31 16:50:01 +0300

    updated the translation routes

--- routes/translations.php
+++ routes/translations.php
@@ -3,20 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 $controllers = [
-    'translations' => '\\' . config('varbox.bindings.controllers.pages_controller', \Varbox\Controllers\TranslationsController::class),
+    'translations' => '\\' . config('varbox.bindings.controllers.translations_controller', \Varbox\Controllers\TranslationsController::class),
 ];
 
 Route::group([
-    'prefix' => 'translations',
+    'prefix' => config('varbox.admin.prefix', 'admin'),
+    'middleware' => [
+        'web',
+        'varbox.auth.session:admin',
+        'varbox.authenticated:admin',
+        'varbox.check.roles',
+        'varbox.check.permissions',
+    ]
 ], function () use ($controllers) {
-    Route::get('/', ['as' => 'admin.translations.index', 'uses' => $controllers['translations'] . '@index', 'permissions' => 'translations-list']);
-    Route::get('create', ['as' => 'admin.translations.create', 'uses' => $controllers['translations'] . '@create', 'permissions' => 'translations-add']);
-    Route::post('store', ['as' => 'admin.translations.store', 'uses' => $controllers['translations'] . '@store', 'permissions' => 'translations-add']);
-    Route::get('edit/{translation}', ['as' => 'admin.translations.edit', 'uses' => $controllers['translations'] . '@edit', 'permissions' => 'translations-edit']);
-    Route::put('update/{translation}', ['as' => 'admin.translations.update', 'uses' => $controllers['translations'] . '@update', 'permissions' => 'translations-edit']);
-    Route::delete('destroy/{translation}', ['as' => 'admin.translations.destroy', 'uses' => $controllers['translations'] . '@destroy', 'permissions' => 'translations-delete']);
-    Route::post('import', ['as' => 'admin.translations.import', 'uses' => $controllers['translations'] . '@import', 'permissions' => 'translations-import']);
-    Route::post('export', ['as' => 'admin.translations.export', 'uses' => $controllers['translations'] . '@export', 'permissions' => 'translations-export']);
-    Route::post('sync', ['as' => 'admin.translations.sync', 'uses' => $controllers['translations'] . '@sync', 'permissions' => 'translations-sync']);
-    Route::delete('clear', ['as' => 'admin.translations.clear', 'uses' => $controllers['translations'] . '@clear', 'permissions' => 'translations-clear']);
+    Route::group([
+        'prefix' => 'translations',
+    ], function () use ($controllers) {
+        Route::get('/', ['as' => 'admin.translations.index', 'uses' => $controllers['translations'] . '@index', 'permissions' => 'translations-list']);
+        Route::get('edit/{translation}', ['as' => 'admin.translations.edit', 'uses' => $controllers['translations'] . '@edit', 'permissions' => 'translations-edit']);
+        Route::put('update/{translation}', ['as' => 'admin.translations.update', 'uses' => $controllers['translations'] . '@update', 'permissions' => 'translations-edit']);
+        Route::delete('destroy/{translation}', ['as' => 'admin.translations.destroy', 'uses' => $controllers['translations'] . '@destroy', 'permissions' => 'translations-delete']);
+        Route::post('import', ['as' => 'admin.translations.import', 'uses' => $controllers['translations'] . '@import', 'permissions' => 'translations-import']);
+        Route::post('export', ['as' => 'admin.translations.export', 'uses' => $controllers['translations'] . '@export', 'permissions' => 'translations-export']);
+        Route::post('sync', ['as' => 'admin.translations.sync', 'uses' => $controllers['translations'] . '@sync', 'permissions' => 'translations-sync']);
+        Route::delete('clear', ['as' => 'admin.translations.clear', 'uses' => $controllers['translations'] . '@clear', 'permissions' => 'translations-delete']);
+    });
 });
