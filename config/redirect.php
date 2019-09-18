f0843303c4d5d24e6a1c9b566882d66df0717a2c
Andrei Badea <zbiller@gmail.com>
2019-09-18 20:00:15 +0300

    added redirect configuration file

---
+++ config/redirect.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |
+    | The redirect statuses that you will use in your application.
+    |
+    */
+    'statuses' => [
+
+        301 => 'Permanent (301)',
+        302 => 'Normal (302)',
+        307 => 'Temporary (307)',
+
+    ],
+
+];
