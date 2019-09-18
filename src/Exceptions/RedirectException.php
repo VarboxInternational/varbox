d46dda9c8e067fdea24695f7a2af085a9be0b56a
Andrei Badea <zbiller@gmail.com>
2019-09-18 20:00:59 +0300

    added a new redirect exception class

---
+++ src/Exceptions/RedirectException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Varbox\Exceptions;
+
+use Exception;
+
+class RedirectException extends Exception
+{
+    /**
+     * The exception to be thrown when the old url is the same as the new url.
+     *
+     * @return static
+     */
+    public static function sameUrls()
+    {
+        return new static('The old url cannot be the same as the new url!');
+    }
+}
