7e9510f849e9a45f11b7d74acb98319ab9fabf3c
Andrei Badea <zbiller@gmail.com>
2019-09-21 17:18:18 +0300

    added support for auto-detecting locales

--- src/Middleware/PersistLocale.php
+++ src/Middleware/PersistLocale.php
@@ -5,6 +5,7 @@ namespace Varbox\Middleware;
 use Closure;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
+use Varbox\Contracts\LanguageModelContract;
 
 class PersistLocale
 {
@@ -14,11 +15,18 @@ class PersistLocale
     protected $app;
 
     /**
-     * @param Application $app
+     * @var LanguageModelContract
      */
-    public function __construct(Application $app)
+    protected $language;
+
+    /**
+     * @param Application $app
+     * @param LanguageModelContract $language
+     */
+    public function __construct(Application $app, LanguageModelContract $language)
     {
         $this->app = $app;
+        $this->language = $language;
     }
 
     /**
@@ -30,6 +38,10 @@ class PersistLocale
     {
         if ($request->session()->has('locale')) {
             $this->app->setLocale($request->session()->get('locale'));
+        } elseif (config('varbox.translation.auto_detect_locale') === true) {
+            $this->app->setLocale($request->getPreferredLanguage(
+                $this->language->onlyActive()->pluck('code')->toArray()
+            ));
         }
 
         return $next($request);
