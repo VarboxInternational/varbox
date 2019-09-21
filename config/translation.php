36eb5be6b57a81013440380bb29a90e76ae6d5dd
Andrei Badea <zbiller@gmail.com>
2019-09-21 17:17:39 +0300

    created the translation config file

---
+++ config/translation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |
+    | Flag whether to automatically detect the user's main browser language and use that as a fallback locale.
+    |
+    | If set to "true", the application will set the locale to the user's most important browser language locale.
+    | Please note that in order for this to happen, you'll need to have at least one "active language" that's also present in the user's browser languages.
+    |
+    | For your information, this logic happens inside the \Varbox\Middleware\PersistLocale middleware.
+    |
+    */
+    'auto_detect_locale' => true,
+
+];
