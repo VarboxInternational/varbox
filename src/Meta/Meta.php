65fdf263198a2741c0cb67bc2955a9a650ed7362
Andrei Badea <zbiller@gmail.com>
2019-10-07 13:50:12 +0300

    added abstract meta class

---
+++ src/Meta/Meta.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Varbox\Meta;
+
+abstract class Meta
+{
+    /**
+     * Build the HTML for the supplied tag keys.
+     *
+     * @param string $key
+     * @param string $value
+     * @return string
+     */
+    abstract public static function tag($key, $value);
+}
