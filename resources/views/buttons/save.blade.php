c8958b054e6536a192872e80e2bc5b1c98f6e760
Andrei Badea <zbiller@gmail.com>
2020-02-29 22:05:39 +0200

    added specific class on save button

--- resources/views/buttons/save.blade.php
+++ resources/views/buttons/save.blade.php
@@ -1,3 +1,3 @@
-<button type="submit" class="btn btn-primary btn-square ml-4" {!! isset($attributes) ? implode(' ', $attributes) : '' !!}>
+<button type="submit" class="button-save btn btn-primary btn-square ml-4" {!! isset($attributes) ? implode(' ', $attributes) : '' !!}>
     <i class="fe fe-check mr-2"></i>Save
 </button>
