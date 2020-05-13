9f7f38b8f04f6391a24e296c55d1581610bf6be5
Andrei Badea <zbiller@gmail.com>
2020-05-13 21:36:23 +0300

    fixed page store under parent route

--- resources/views/admin/pages/add.blade.php
+++ resources/views/admin/pages/add.blade.php
@@ -3,5 +3,5 @@
 @section('title', $title)
 
 @section('content')
-    @include('varbox::admin.pages._form', ['url' => route('admin.pages.store', ['parent' => $parent ?: null])])
+    @include('varbox::admin.pages._form', ['url' => route('admin.pages.store', ['pageParent' => $parent ?: null])])
 @endsection
