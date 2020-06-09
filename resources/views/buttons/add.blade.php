8260a13e4a020c66338ab65e1f7f5530dfa86b6d
Andrei Badea <zbiller@gmail.com>
2020-06-09 15:38:25 +0300

    updated html for the add blade button

--- resources/views/buttons/add.blade.php
+++ resources/views/buttons/add.blade.php
@@ -1,7 +1,3 @@
-<div class="card">
-    <div class="card-body">
-        <a href="{{ $url }}" class="button-add btn btn-primary btn-square btn-block" {!! isset($attributes) ? implode(' ', $attributes) : '' !!}>
-            <i class="fe fe-plus mr-2"></i>Add New
-        </a>
-    </div>
-</div>
+<a href="{{ $url }}" class="button-add btn btn-primary btn-square btn-block" {!! isset($attributes) ? implode(' ', $attributes) : '' !!}>
+    <i class="fe fe-plus mr-2"></i>Add New
+</a>
